20a6b2efb9e9199e67f769aa7c82f5cb2b802d9d
Robert Obkircher <robert.obkircher@gmail.com>
2019-01-19 13:02:01 +0100

    render something

---
+++ assets/shaders/shader1.glsl
@@ -0,0 +1,60 @@
+// https://learnopengl.com/Advanced-OpenGL/Geometry-Shader
+
+#ifdef VertexShader
+
+layout (location = 0) in float aPos;
+layout (location = 1) in float aSpeed;
+
+out VS_OUT {
+  vec3 color;
+} vs_out;
+
+void main()
+{
+    gl_Position = vec4(2 * aPos - 1, 0.0, 0.0, 1.0);
+    float base = 0.4;
+    float maxSpeed = 5.0;
+    float scale = (1 - base) / maxSpeed;
+    vs_out.color = vec3(base, base, base) + scale * vec3(aSpeed, 0.0, aSpeed / 2.0);
+}
+
+#endif
+
+#ifdef GeometryShader
+
+in VS_OUT {
+  vec3 color;
+} gs_in[];
+out vec3 fColor;
+
+layout (points) in;
+layout (triangle_strip, max_vertices = 4) out;
+
+void main() {
+  float size = 0.05;
+  fColor = gs_in[0].color;
+  gl_Position = gl_in[0].gl_Position + vec4(-size, -size, 0.0, 0.0);
+  EmitVertex();
+  gl_Position = gl_in[0].gl_Position + vec4(size, -size, 0.0, 0.0);
+  EmitVertex();
+  gl_Position = gl_in[0].gl_Position + vec4(-size, size, 0.0, 0.0);
+  EmitVertex();
+  gl_Position = gl_in[0].gl_Position + vec4(size, size, 0.0, 0.0);
+  EmitVertex();
+  EndPrimitive();
+}
+
+
+#endif
+
+#ifdef FragmentShader
+
+in vec3 fColor;
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(fColor, 1.0);
+}
+
+#endif
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
   ( someFunc
   ) where
 
+import Control.Monad
+
 import Control.Monad.Loops
+import qualified Data.ByteString as B
+import Foreign.Marshal.Array
+import Foreign.Ptr
+import qualified Graphics.Rendering.OpenGL as GL
+import Graphics.Rendering.OpenGL (($=))
 import qualified Graphics.UI.GLFW as GLFW
+import System.Exit (exitFailure)
 import System.IO
 
 someFunc :: IO ()
@@ -13,7 +23,20 @@ someFunc = do
   GLFW.windowHint $ GLFW.WindowHint'DepthBits (Just 16)
   Just window <- GLFW.createWindow 800 600 "cars" Nothing Nothing
   GLFW.makeContextCurrent (Just window)
+  shader1 <- loadShader1
+  GL.currentProgram $= Just shader1
+
+  b <- GL.genObjectName
+  GL.bindBuffer GL.ArrayBuffer $= Just b
+  GL.vertexAttribArray (GL.AttribLocation 0) $= GL.Enabled
+  GL.vertexAttribArray (GL.AttribLocation 1) $= GL.Enabled
+
   whileM_ (not <$> GLFW.windowShouldClose window) $ do
+    withArrayLen [0.3 :: Float, 0.0, 0.7, 2.0, 0.9, 5.0] $ \count arr ->
+      GL.bufferData GL.ArrayBuffer $= (fromIntegral count * 4, arr, GL.StreamDraw)
+    GL.vertexAttribPointer (GL.AttribLocation 0) $= (GL.ToFloat, GL.VertexArrayDescriptor 1 GL.Float 8 nullPtr)
+    GL.vertexAttribPointer (GL.AttribLocation 1) $= (GL.ToFloat, GL.VertexArrayDescriptor 1 GL.Float 8 (plusPtr nullPtr 4))
+    GL.drawArrays GL.Points 0 3
     GLFW.swapBuffers window
     GLFW.pollEvents
   GLFW.destroyWindow window
@@ -21,3 +44,31 @@ someFunc = do
 
 errorCallback :: GLFW.ErrorCallback
 errorCallback _ = hPutStrLn stderr
+
+loadShader1 :: IO GL.Program
+loadShader1 = do
+  text <- B.readFile "assets/shaders/shader1.glsl"
+  vert <- makeShader GL.VertexShader text
+  geom <- makeShader GL.GeometryShader text
+  frag <- makeShader GL.FragmentShader text
+  program <- GL.createProgram
+  mapM_ (GL.attachShader program) [vert, geom, frag]
+  GL.linkProgram program
+  linked <- GL.get (GL.linkStatus program)
+  GL.validateProgram program
+  valid <- GL.get (GL.validateStatus program)
+  unless (linked && valid) $ GL.get (GL.programInfoLog program) >>= logAndExit
+  return program
+
+makeShader :: GL.ShaderType -> B.ByteString -> IO GL.Shader
+makeShader shaderType text = do
+  s <- GL.createShader shaderType
+  GL.shaderSourceBS s $=
+    B.concat ["#version 330 core\n#define ", GL.packUtf8 (show shaderType), "\n", text]
+  GL.compileShader s
+  success <- GL.get (GL.compileStatus s)
+  unless success $ GL.get (GL.shaderInfoLog s) >>= logAndExit
+  return s
+
+logAndExit :: String -> IO ()
+logAndExit s = hPutStrLn stderr s >> exitFailure
