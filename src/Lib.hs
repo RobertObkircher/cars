08ff3a9ab0cadc043da9931ddf0f1e5de3e12956
Robert Obkircher <robert.obkircher@gmail.com>
2019-01-19 10:00:46 +0100

    window

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+import Control.Monad.Loops
+import qualified Graphics.UI.GLFW as GLFW
+import System.IO
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  GLFW.setErrorCallback (Just errorCallback)
+  True <- GLFW.init
+  GLFW.windowHint $ GLFW.WindowHint'DepthBits (Just 16)
+  Just window <- GLFW.createWindow 800 600 "cars" Nothing Nothing
+  GLFW.makeContextCurrent (Just window)
+  whileM_ (not <$> GLFW.windowShouldClose window) $ do
+    GLFW.swapBuffers window
+    GLFW.pollEvents
+  GLFW.destroyWindow window
+  GLFW.terminate
+
+errorCallback :: GLFW.ErrorCallback
+errorCallback _ = hPutStrLn stderr
